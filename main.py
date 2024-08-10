c76df05c8ceb7c725b49dcda45d35fa391e064a0
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2024-08-11 01:24:10 +0200

    new constants added

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import analyser
 
 AD_TYPES = ['prodaja', 'oddaja']
 REAL_ESTATE_TYPES = ['stanovanje', 'hisa']
+MAIN_PAGE = 'https://www.nepremicnine.net/'
+REGIONS = ['LJ-mesto', 'LJ-okolica', 'Gorenjska', 'J. Primorska', 'S.Primorska', 'Notranjska', 'Savinjska', 'Podravska', 'Koroška', 'Dolenjska', 'Posavska', 'Zasavska', 'Pomurska']
 
 
 def main():
-    pass
+    collector.save_page_to_file(MAIN_PAGE, 'pages', 'main_page')
 
 if __name__ == '__main__':
     main()
