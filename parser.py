1a6e3fe12ca1d7ef5a20ac04ca5c3b46493301b2
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2024-08-11 06:04:11 +0200

    added function implementation that gets the number of pages by given criteria.

--- parser.py
+++ parser.py
@@ -21,3 +21,20 @@ def get_filename(url):
     return filename[:-1]
 
 
+# Main functions
+def get_number_of_pages(url_base):
+    filename = get_filename(url_base) + '_1'
+    collector.save_page_to_file(url_base, directory=DIRECTORY, filename=filename)
+    text = read_file_to_string(directory=DIRECTORY, filename=filename)
+    number_of_pages = get_number_of_pages_from_text(text)
+    return number_of_pages
+    
+
+def get_number_of_pages_from_text(text):
+    template_number_of_pages = r'<li class="paging_last"><a href=(.*?) class="last">>></a></li>'
+    number_of_pages = re.search(template_number_of_pages, text)
+    
+    if number_of_pages != None:
+        return int(number_of_pages.group(1).split('/')[4])
+    else:
+        return 1
