f45825708012bc3878aada7095f493acd4c38654
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2024-08-11 02:17:57 +0200

    added implementation of preparing links for downloading

---
+++ processor.py
@@ -0,0 +1,39 @@
+import collector
+
+
+def get_first_page(ad_type, real_estate_type, region, main_page):
+    return f'{main_page}oglasi-{ad_type}/{region}/{real_estate_type}/'
+
+def get_first_pages(ad_types, real_estate_types, regions, main_page):
+    first_pages = set()
+    for ad_type in ad_types:
+        for real_estate_type in real_estate_types:
+            for region in regions:
+                first_page = get_first_page(ad_type, real_estate_type, region, main_page)
+                first_pages.add(first_page)
+    return first_pages
+
+def get_number_of_pages(first_page):
+    filename = get_filename(first_page)
+    collector.save_page_to_file(first_page, 'pages', filename)
+    return 1
+
+def get_pages_to_download(ad_types, real_estate_types, regions, main_page):
+    pages_to_download = set()
+    
+    first_pages = get_first_pages(ad_types, real_estate_types, regions, main_page)
+    for first_page in first_pages:
+        number_of_pages = get_number_of_pages(first_page)
+        for number in range(1, number_of_pages + 1):
+            page_to_download = f'{first_page}{number}/'
+            pages_to_download.add(page_to_download)    
+    
+    return pages_to_download
+
+
+def get_filename(link):
+    filename = ''
+    for data in link.split('/')[3:-1]:
+        filename += data.lower() + '_'
+
+    return filename[:-1]
