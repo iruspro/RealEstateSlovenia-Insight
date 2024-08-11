a2208a486a3e73aaff68aede9f8df4dc6d7dff35
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2024-08-11 02:17:14 +0200

    add a realization of basic function

--- collector.py
+++ collector.py
@@ -1,10 +1,39 @@
+import requests
+import os
+
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36', 
+            'authority': 'www.nepremicnine.net'}
+
+
 def download_url_to_string(url):
-    pass
+    """The function takes a string as an argument and tries to return the contents of this web
+    as a string. If an error occurs during execution, it returns None.
+    """
+
+    try:
+        r = requests.get(url, headers=HEADERS)
+        code = r.status_code
+        if code != 200:
+            print(code, url)
+        text = r.text
+        return text
+    except:
+        return None
 
 
 def save_string_to_file(text, directory, filename):
-    pass
+    """The function writes the value of the "text" parameter to a newly created file
+    located in "directory"/"filename", or overwrites an existing one. In case
+    the "directory" string is empty, the file is created in the current directory.
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None
 
 
 def save_page_to_file(page, directory, filename):
-    pass
+    text = download_url_to_string(page)
+    save_string_to_file(text, directory, filename)
