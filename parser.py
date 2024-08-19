163977840bfef2384a45b53a3627a1553b1edfbb
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2024-08-19 03:53:46 +0200

    code refactoring

--- parser.py
+++ parser.py
@@ -71,45 +71,39 @@ def get_dict_from_ad_block(block):
         return None
 
     ad_id = get_ad_id_from_(block)
-    type_ = get_type_from_(block)    
-    land_area = get_land_area_from_(block)
     living_area = get_living_area_from_(block)
-    building_year = get_building_year_from_(block)
-    floor = get_floor_from_(block)
-    price = get_price_from_(block)
-    seller = get_seller_from_(block)
     city = get_city_from_(block, base_data[1])
-
-    if ad_id == None or city == None or living_area == None or price == None:
+    if not ad_id or not city or not living_area:
+        return None      
+    
+    price = get_price_from_(block, living_area)
+    if not price:
         return None  
 
+    building_year = get_building_year_from_(block)
+    floor = get_floor_from_(block)
+    type_ = get_type_from_(block)
+    land_area = get_land_area_from_(block)    
+    seller = get_seller_from_(block)    
+
     return {
-        'ad_id': ad_id,
+        'id': ad_id,
         'ad_type': base_data[0],
+        'real_estate_type': base_data[2],
         'region': base_data[1],
-        'real_estate_type': base_data[2],               
-        'type': type_,
-        'land_area': land_area,
         'city': city,
-        'living_area': living_area,
+        'type': type_,
+        'living_area': living_area, 
+        'land_area': land_area,
+        'floor': floor,                 
         'building_year': building_year,
-        'floor': floor,
         'price': price,
         'seller': seller,
     }
 
 
-def was_found(match):
-    """The function takes a search object as an argument and returns its contents 
-    or None if no match was found."""
-    if match == None:
-        return None
-    else:
-        return match.group(1).lower()
-
-
 def get_base_date_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns basic data about it or None if there is no necessery data in the block."""
     template_base_data = r'<meta itemprop="category" content="(.*?)/>'
     base_data = re.search(template_base_data, block)
@@ -128,78 +122,97 @@ def get_base_date_from_(block):
 
 
 def get_ad_id_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns ad id or None if there is no ad id in the block."""
     template_id = r'<div class="property-details".*?_(.*?)/">'
     ad_id = re.search(template_id, block)
-    return was_found(ad_id)
+    if ad_id:
+        return ad_id.group(1).lower()
 
 
 def get_type_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns type or None if there is no type in the block."""
     template_type = r'<span class="tipi">(.*?)</span>'
     type_ = re.search(template_type, block)
-    return was_found(type_)
+    if type_:
+        return type_.group(1).lower()
 
 
 def get_land_area_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns land area or None if there is no land area in the block."""
     template_land_area = r'<li><img src="https://www\.nepremicnine\.net/images/zemljisce\.svg".*?>(.*?) m<sup>2</sup></li>'
     land_area = re.search(template_land_area, block)
-    return was_found(land_area)
+    if land_area:
+        return float(land_area.group(1).lower())
 
 
 def get_city_from_(block, region):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns city or None if there is no city in the block."""
     if region == 'ljubljana mesto':
         return 'ljubljana'
     else:     
         template_city = r'<h2>(.*?)</h2>'
         city = re.search(template_city, block)
-        if city == None:
-            return None
-        else:
+        if city:
             return city.group(1).split(',')[0].lower()
 
 
 def get_living_area_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns living area or None if there is no living area in the block."""
     template_living_area = r'<li><img src="https://www\.nepremicnine\.net/images/velikost\.svg".*?>(.*?) m<sup>2</sup></li>'
     living_area = re.search(template_living_area, block)
-    return was_found(living_area)
+    if living_area:
+        living_area = float(living_area.group(1).lower().replace('.', '').replace(',', '.'))
+        return living_area
 
 
 def get_building_year_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns building year or None if there is no building year in the block."""
     template_building_year = r'<li><img src="https://www\.nepremicnine\.net/images/leto\.svg".*?>(.*?)</li>'
     building_year = re.search(template_building_year, block)
-    return was_found(building_year)
+    if building_year:
+        return int(building_year.group(1).lower())
 
 
 def get_floor_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns floor or None if there is no floor in the block."""
     template_floor = r'<li><img src="https://www\.nepremicnine\.net/images/nadstropje\.svg".*?>(.*?)</li>'
     floor = re.search(template_floor, block)
-    return was_found(floor)
+    if floor:
+        return floor.group(1).lower()
 
 
-def get_price_from_(block):    
-    """The function takes a block with a ad as an argument 
+def get_price_from_(block, living_area):    
+    """The function takes a block with an ad and living area as an argument 
     and returns price or None if there is no price in the block."""
-    template_price = r'<meta itemprop="price" content="(.*?)" />'
+    template_price = r'<h6 class="">(.*?)</h6>'
     price = re.search(template_price, block)
-    return was_found(price)
+    if price:
+        price = price.group(1).lower()
+
+        if price == 'cena ni določena':
+            return None
+
+        price_data = price.split(' ')
+        price = float(price_data[0].replace('.', '').replace(',', '.'))   
+        if '/dan' in price_data[1] or '/teden' in price_data[1]:
+            return None
+        elif '/m2' in price_data[1]:
+            return price * living_area
+        else:
+            return price
 
 
 def get_seller_from_(block):
-    """The function takes a block with a ad as an argument 
+    """The function takes a block with an ad as an argument 
     and returns seller or None if there is no seller in the block."""
     template_seller = r'<meta itemprop="name" content="(.*?)" />'
     seller = re.search(template_seller, block)
-    return was_found(seller)
+    if seller:
+        return seller.group(1).lower()
